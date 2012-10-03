9784d8dc47566f8bef93720530a27630ada7a37a
Oliver Wickham <mr.o.wickham@gmail.com>
2012-10-03 22:06:48 +0100

    Added tests to check that game has the correct parameters

--- paper-stone-scissors/javascript/spec/FirstToGameSpec.js
+++ paper-stone-scissors/javascript/spec/FirstToGameSpec.js
@@ -20,4 +20,41 @@ describe("FirstToGame", function() {
     game.registerWinForPlayer(1);
     expect(gameOverCallback).toHaveBeenCalled();
   });
+  
+  it("should not allow any further scores after completion", function() {
+    var game = new FirstToGame({
+        winningScore: 1,
+        numberOfPlayers: 2
+    });
+    
+    game.registerWinForPlayer(1);
+    
+    expect(function () { game.registerWinForPlayer(1); }).toThrow('Game Over');
+  });
+  
+  it("should only allow games that have 2 or more players", function() {
+    
+    expect(function () { 
+        new FirstToGame({
+            numberOfPlayers: 0
+        });
+    }).toThrow('Game must have at least 2 players');
+  });
+  
+  it("should only allow winning scores greater than 0", function() {
+    
+    expect(function () { 
+        new FirstToGame({
+            numberOfPlayers: 2,
+            winningScore: 0
+        });
+    }).toThrow('Game must have at winning scores greater than 0');
+    
+    expect(function () { 
+        new FirstToGame({
+            numberOfPlayers: 2,
+            winningScore: -1
+        });
+    }).toThrow('Game must have at winning scores greater than 0');
+  });
 });
--- paper-stone-scissors/javascript/src/FirstToGame.js
+++ paper-stone-scissors/javascript/src/FirstToGame.js
@@ -9,18 +9,41 @@
         }
     }
     
+    function validateOptions(options)
+    {
+        if (typeof options.numberOfPlayers !== 'number'
+            || options.numberOfPlayers < 2) {
+                throw 'Game must have at least 2 players';
+            }
+            
+        if (typeof options.winningScore !== 'number'
+            || options.winningScore < 1) {
+                throw 'Game must have at winning scores greater than 0';
+            }
+    }
+    
     function FirstToGame(options) {
+        
+        validateOptions(options);
+        
         this.options = options;
+        this.completed = false;
+        
         this.playerScores = [this.options.numberOfPlayers];
         setAllArrayValues(this.playerScores, 0);
     }
 
     FirstToGame.prototype.registerWinForPlayer = function(playerNumber) {
+        if (this.completed) {
+            throw 'Game Over';
+        }
+        
         var index = playerNumber - 1;
     
         this.playerScores[index]++;
         
-        if (this.playerScores[index] == this.options.winningScore) {
+        if (this.playerScores[index] === this.options.winningScore) {
+            this.completed = true;
             if (this.options.gameOverCallback) {
                 this.options.gameOverCallback();
             }
