d60d202b28295517ec8a2d85ef33ee9ec047ba42
Oliver Wickham <mr.o.wickham@gmail.com>
2012-10-03 22:43:01 +0100

    Refactor the structure of the describe blocks

--- paper-stone-scissors/javascript/spec/FirstToGameSpec.js
+++ paper-stone-scissors/javascript/spec/FirstToGameSpec.js
@@ -1,60 +1,66 @@
-describe("FirstToGame", function() {  
-  it("should finish when one player scores the winning score", function() {
-    var gameOverCallback = jasmine.createSpy();
-    
-    var game = new FirstToGame({
-        winningScore: 3,
-        numberOfPlayers: 2,
-        gameOverCallback: gameOverCallback
-    });
-    
-    game.registerWinForPlayer(1);
-    expect(gameOverCallback).not.toHaveBeenCalled();
-    
-    game.registerWinForPlayer(1);
-    expect(gameOverCallback).not.toHaveBeenCalled();
-    
-    game.registerWinForPlayer(2);
-    expect(gameOverCallback).not.toHaveBeenCalled();
-    
-    game.registerWinForPlayer(1);
-    expect(gameOverCallback).toHaveBeenCalled();
-  });
-  
-  it("should not allow any further scores after completion", function() {
-    var game = new FirstToGame({
-        winningScore: 1,
-        numberOfPlayers: 2
-    });
-    
-    game.registerWinForPlayer(1);
-    
-    expect(function () { game.registerWinForPlayer(1); }).toThrow('Game Over');
-  });
-  
-  it("should only allow games that have 2 or more players", function() {
-    
-    expect(function () { 
-        new FirstToGame({
-            numberOfPlayers: 0
-        });
-    }).toThrow('Game must have at least 2 players');
-  });
-  
-  it("should only allow winning scores greater than 0", function() {
-    
-    expect(function () { 
-        new FirstToGame({
+describe("FirstToGame", function() {
+    describe("Creating a Game", function() {
+       it("should only allow games that have 2 or more players", function() {
+          
+          expect(function () { 
+                 new FirstToGame({
+                                 numberOfPlayers: 0
+                                 });
+                 }).toThrow('Game must have at least 2 players');
+       });
+       
+       it("should only allow winning scores greater than 0", function() {
+          
+          expect(function () { 
+                 new FirstToGame({
+                                 numberOfPlayers: 2,
+                                 winningScore: 0
+                                 });
+                 }).toThrow('Game must have at winning scores greater than 0');
+          
+          expect(function () { 
+                 new FirstToGame({
+                                 numberOfPlayers: 2,
+                                 winningScore: -1
+                                 });
+                 }).toThrow('Game must have at winning scores greater than 0');
+       });
+   });
+         
+  describe("Game Over Callback", function() {
+      it("should fire when one player scores the winning score", function() {
+        var gameOverCallback = jasmine.createSpy();
+        
+        var game = new FirstToGame({
+            winningScore: 3,
             numberOfPlayers: 2,
-            winningScore: 0
+            gameOverCallback: gameOverCallback
         });
-    }).toThrow('Game must have at winning scores greater than 0');
-    
-    expect(function () { 
-        new FirstToGame({
-            numberOfPlayers: 2,
-            winningScore: -1
+        
+        game.registerWinForPlayer(1);
+        expect(gameOverCallback).not.toHaveBeenCalled();
+        
+        game.registerWinForPlayer(1);
+        expect(gameOverCallback).not.toHaveBeenCalled();
+        
+        game.registerWinForPlayer(2);
+        expect(gameOverCallback).not.toHaveBeenCalled();
+        
+        game.registerWinForPlayer(1);
+        expect(gameOverCallback).toHaveBeenCalled();
+      });
+   });
+   
+   describe("Registering a Win for Player", function() {
+      it("should not allow any further scores after completion", function() {
+        var game = new FirstToGame({
+            winningScore: 1,
+            numberOfPlayers: 2
         });
-    }).toThrow('Game must have at winning scores greater than 0');
-  });
-});
+        
+        game.registerWinForPlayer(1);
+        
+        expect(function () { game.registerWinForPlayer(1); }).toThrow('Game Over');
+      });
+   });
+ });
--- paper-stone-scissors/javascript/spec/RoundSpec.js
+++ paper-stone-scissors/javascript/spec/RoundSpec.js
@@ -1,8 +1,22 @@
-describe("Round", function() {  
-  it("should choose scissors as the winner above stone", function() {
+describe("Round", function() {
+ describe("Selecting a winner", function() {
+  it("should choose stone above scissors", function() {
+    var round = new Round();
+    round.registerChoice(1, "stone");
+    round.registerChoice(2, "scissors");
+    expect(round.selectWinner()).toEqual(1);
+  });
+  it("should choose scissors above paper", function() {
     var round = new Round();
     round.registerChoice(1, "scissors");
+    round.registerChoice(2, "paper");
+    expect(round.selectWinner()).toEqual(1);
+  });
+  it("should choose paper above stone", function() {
+    var round = new Round();
+    round.registerChoice(1, "paper");
     round.registerChoice(2, "stone");
     expect(round.selectWinner()).toEqual(1);
   });
+ });
 });
