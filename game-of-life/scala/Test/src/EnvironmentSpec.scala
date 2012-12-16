e0c1949e8d9579c4f46d4b6a1b86003dc6e1c05f
Oliver Wickham <oliver.wickham@argusmedia.com>
2012-12-16 01:05:30 +0000

    Add some test classes

---
+++ game-of-life/scala/Test/src/EnvironmentSpec.scala
@@ -0,0 +1,24 @@
+package gameoflife.tests
+
+import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers
+
+class Environment
+
+class EnvironmentSpec extends FunSuite with ShouldMatchers {
+
+	/*describe("A Stack") {
+
+	    it("should pop values in last-in-first-out order") {
+	      //val stack = new Stack[Int]
+	      //stack.push(1)
+	      //stack.push(2)
+	      //assert(stack.pop() === 2)
+	      //assert(stack.pop() === 1)
+	    }
+	/*"An Environment" should "be constructable" in {
+	  //val environment = Environment()
+	 //environment.getClass().toString() should startWith ("Environment")
+	}*/
+	}*/
+}
---
+++ game-of-life/scala/Test/src/Test.scala
@@ -0,0 +1,5 @@
+import gameoflife.tests._
+
+object Test extends App {
+	val env = new Environment()
+}
