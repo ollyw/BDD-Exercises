e89076e9ba5ace4a08d4dfe7f4595e216f004353
Oliver Wickham <mr.o.wickham@gmail.com>
2013-01-09 15:45:17 +0000

    Allow for dynamic changing of grid size during rendering and fix a couple of bugs

--- game-of-life/scala/GameOfLifeWeb/public/javascripts/backbone/src/Main.js
+++ game-of-life/scala/GameOfLifeWeb/public/javascripts/backbone/src/Main.js
@@ -2,17 +2,15 @@
 (function () {
 	var container,
 		model = new BoardModel(),
-		board;
+		board,
+		timer,
+		interval = 750;
 	
 	board = new BoardWidget({ model: model });
 	
 	$(function () {
 		
 		container = $('#testBoard');
-		
-		model.on('reset', function () {
-			container.append(board.render().$el);
-		});
 		
 		$('#newGame').click(newGame);
 		$('#replayGame').click(replayGame);
@@ -22,28 +20,36 @@
 	function newGameSuccess(data) {
 		model.start(data);
 		container.append(board.render().$el);
-		setTimeout(updateToNextGeneration, 1500)
+		timer = setTimeout(updateToNextGeneration, interval)
 	}
 	
 	function replayGame() {
+		clearInterval(timer);
+		updateBoardDimensions();
+		
 		model.replay();
 		container.append(board.render().$el);
-		setTimeout(updateToNextGeneration, 1500);
+		timer = setTimeout(updateToNextGeneration, interval);
 	}
 	
-	function newGame() {
+	function updateBoardDimensions() {
 		var rows = $('input[name=rows]').val(),
 		columns= $('input[name=columns]').val();
 		
 		model.set('rows', rows);
 		model.set('columns', columns);
+	}
+	
+	function newGame() {
+		clearInterval(timer);
 		
+		updateBoardDimensions();
 		var data = model.toJSON();
 		
 		$.ajax({
 			  type: 'POST',
 			  url: model.url,
-			  data: JSON.stringify({ "rows": rows, "columns": columns }),
+			  data: JSON.stringify({ "rows": model.get('rows'), "columns": model.get('columns') }),
 			  success: newGameSuccess,
 			  dataType: "json",
 			  contentType: 'application/json'
@@ -51,11 +57,15 @@
 	}
 	
 	function exampleGame() {
+		clearInterval(timer);
+		
 		var sample = [{"row":4,"column":14},{"row":10,"column":17},{"row":14,"column":1},{"row":5,"column":10},{"row":12,"column":1},{"row":13,"column":17},{"row":2,"column":17},{"row":4,"column":15},{"row":12,"column":11},{"row":8,"column":16},{"row":8,"column":15},{"row":2,"column":7},{"row":7,"column":15},{"row":8,"column":14},{"row":3,"column":8},{"row":9,"column":19},{"row":5,"column":3},{"row":10,"column":15},{"row":9,"column":14},{"row":10,"column":18}]
 		model.set('rows', 20);
 		model.set('columns', 40);
 		model.get('seedCells').reset(sample);
 		model.replay();
+		
+		timer = setTimeout(updateToNextGeneration, interval)
 	}
 	
 	function updateToNextGeneration() {
@@ -72,8 +82,9 @@
 	
 	function updateToNextGenerationSuccess(data) {
 		model.update(data);
+		updateBoardDimensions();
 		container.append(board.render().$el);
-		setTimeout(updateToNextGeneration, 1500)
+		timer = setTimeout(updateToNextGeneration, interval)
 	}
 	
 })()
