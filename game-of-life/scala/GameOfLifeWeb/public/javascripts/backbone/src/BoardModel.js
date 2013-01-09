a144f102bf0ffd5443f0617d25590423bfdea669
Oliver Wickham <mr.o.wickham@gmail.com>
2013-01-09 12:04:23 +0000

    Get the replay mechanism working, and start cleaning up the js (a bit)

--- game-of-life/scala/GameOfLifeWeb/public/javascripts/backbone/src/BoardModel.js
+++ game-of-life/scala/GameOfLifeWeb/public/javascripts/backbone/src/BoardModel.js
@@ -1,4 +1,19 @@
 var BoardModel = Backbone.Model.extend({
+	
 	url: 'http://localhost:9000/gameoflife/newgame',
-	defaults: { rows: null, columns: null, aliveCells: new CellsCollection() }
+	
+	defaults: { rows: null, columns: null, aliveCells: new CellsCollection(), seedCells: new CellsCollection() },
+	
+	start: function (seedCells) {
+		this.get('seedCells').reset(seedCells);
+		this.get('aliveCells').reset(seedCells);
+	},
+	
+	update: function (newCells) {
+		this.get('aliveCells').reset(newCells);
+	},
+	
+	replay: function () {
+		this.get('aliveCells').reset(this.get('seedCells').toJSON())
+	}
 });
--- game-of-life/scala/GameOfLifeWeb/public/javascripts/backbone/src/Main.js
+++ game-of-life/scala/GameOfLifeWeb/public/javascripts/backbone/src/Main.js
@@ -2,10 +2,8 @@
 (function () {
 	var container,
 		model = new BoardModel(),
-		cellsCollection = new CellsCollection(),
 		board;
 	
-	model.set('aliveCells',  cellsCollection)
 	board = new BoardWidget({ model: model });
 	
 	$(function () {
@@ -16,13 +14,21 @@
 			container.append(board.render().$el);
 		});
 		
-		$('button').click(newGame);
+		$('#newGame').click(newGame);
+		$('#replayGame').click(replayGame);
+		$('#exampleGame').click(exampleGame);
 	});
 	
 	function newGameSuccess(data) {
-		cellsCollection.reset(data);
+		model.start(data);
 		container.append(board.render().$el);
-		setTimeout(updateToNextGeneration, 1000)
+		setTimeout(updateToNextGeneration, 1500)
+	}
+	
+	function replayGame() {
+		model.replay();
+		container.append(board.render().$el);
+		setTimeout(updateToNextGeneration, 1500);
 	}
 	
 	function newGame() {
@@ -44,11 +50,19 @@
 			});
 	}
 	
+	function exampleGame() {
+		var sample = [{"row":4,"column":14},{"row":10,"column":17},{"row":14,"column":1},{"row":5,"column":10},{"row":12,"column":1},{"row":13,"column":17},{"row":2,"column":17},{"row":4,"column":15},{"row":12,"column":11},{"row":8,"column":16},{"row":8,"column":15},{"row":2,"column":7},{"row":7,"column":15},{"row":8,"column":14},{"row":3,"column":8},{"row":9,"column":19},{"row":5,"column":3},{"row":10,"column":15},{"row":9,"column":14},{"row":10,"column":18}]
+		model.set('rows', 20);
+		model.set('columns', 40);
+		model.get('seedCells').reset(sample);
+		model.replay();
+	}
+	
 	function updateToNextGeneration() {
-		var data = cellsCollection.toJSON();
+		var data = model.get('aliveCells').toJSON();
 		$.ajax({
 			  type: 'POST',
-			  url: cellsCollection.url,
+			  url: model.get('aliveCells').url,
 			  data: JSON.stringify(data),
 			  success: updateToNextGenerationSuccess,
 			  dataType: "json",
@@ -57,9 +71,9 @@
 	}
 	
 	function updateToNextGenerationSuccess(data) {
-		cellsCollection.reset(data);
-		setTimeout(updateToNextGeneration, 1000)
+		model.update(data);
 		container.append(board.render().$el);
+		setTimeout(updateToNextGeneration, 1500)
 	}
 	
 })()
