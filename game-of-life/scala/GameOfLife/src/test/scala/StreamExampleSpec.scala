705e89e7df59ad5f8e830ea7755d0ddbe5fe4e9d
Oliver Wickham <mr.o.wickham@gmail.com>
2013-04-02 23:02:46 +0200

    Update Given/When/Then to new uppercase format

--- game-of-life/scala/GameOfLife/src/test/scala/StreamExampleSpec.scala
+++ game-of-life/scala/GameOfLife/src/test/scala/StreamExampleSpec.scala
@@ -11,13 +11,13 @@ class StreamExampleSpec extends FeatureSpec with GivenWhenThen with ShouldMatche
 	ignore("Scala lazy evaluation with streams") {
 		scenario("Recursive call doesn't cause a stack overflow") {
 		  
-			given("a function which calculation which runs the square of integers")
+			Given("a function which calculation which runs the square of integers")
 			def calcSquare(v: => BigInt) : Stream[BigInt] = (v * v) #:: calcSquare(v + 1)
 			
-			when("when pop is invoked on the stack")
+			When("when pop is invoked on the stack")
 			val squareStream = calcSquare(0) take 4
 			
-			then("the third value should be 9 (3 x 3) ")
+			Then("the third value should be 9 (3 x 3) ")
 			squareStream.size should equal(4)
 			squareStream.last should equal(9)
 		}
